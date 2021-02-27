cc18a7b2dc575dc25e4fbca285fdaafcda3c3413
EC2 Default User <ec2-user@ip-172-31-34-110.us-east-2.compute.internal>
2021-02-27 06:56:05 +0000

    add streets csv again

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require "csv"
 
-streets_csv = CSV.readlines("db/street.csv")
+streets_csv = CSV.readlines("db/streets.csv")
 streets_csv.each do |row|
   Street.create(street_name: row[1], side0: row[2], side1: row[3])
 end
