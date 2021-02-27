869807dad9fd9f1fd1e602f4c8dfdfc690a53e01
EC2 Default User <ec2-user@ip-172-31-34-110.us-east-2.compute.internal>
2021-02-27 05:36:30 +0000

    add streets seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,1 @@
-Street.create(street_name: 'PropellerSt-プロペラ通り',side0:0,side1:1)
-Street.create(street_name: 'OmotesandoSt-表参道(1)',side0:0,side1:1)
-Street.create(street_name: 'OmotesandoSt-表参道(2)',side0:0,side1:1)
+Street.create(street_name: 'PropellerSt-プロペラ通り', side0:'0', side1:'1')
