099565d731f75b6a0203c2976b0242ccd4337d1e
EC2 Default User <ec2-user@ip-172-31-34-110.us-east-2.compute.internal>
2021-03-17 06:21:04 +0000

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require "csv"
 
-events_csv = CSV.readlines("db/events.csv")
-events_csv.each do |row|
-  Event.create(event_title: row[1], goal: row[2], event_url: row[3])
+shops_csv = CSV.readlines("db/shops.csv")
+shops_csv.each do |row|
+  Shop.create(location_id: row[1], shop_name: row[2], category: row[3], url: row[4], logo_url: row[5], floor: row[6])
 end
