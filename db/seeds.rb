83a9500fea22ccec4bc54a0ade64316e547edf3f
EC2 Default User <ec2-user@ip-172-31-34-110.us-east-2.compute.internal>
2021-03-21 03:37:54 +0000

    add locations csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require "csv"
 
-operationhours_csv = CSV.readlines("db/operationhours.csv")
-    operationhours_csv.each do |row|
-        OperationHour.create(shop_id: row[1], day: row[2], open1: row[3], close1: row[4], open2: row[5], close2: row[6])
-    end
+    locations_csv = CSV.readlines("db/locations.csv")
+        locations_csv.each do |row|
+            Location.create(location: row[1], street_id: row[2], side: row[3])
+        end
