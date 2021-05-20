6655020693fa3b766878b5b5da6678ad88e27187
EC2 Default User <ec2-user@ip-172-31-34-110.us-east-2.compute.internal>
2021-05-20 07:44:54 +0000

    edit location controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -32,14 +32,18 @@ class LocationsController < ApplicationController
 				p "連番発生してます"
 				@renban_next=@streetSide.street_name.next #＠renban="Omotesando-表参道(3)"
 				p @renban_next=Street.find_by(street_name: @renban_next)
-				if @renban_next.blank?#nil? #あんぱ？
-					p "ないなら、prev連番"
+#				if @renban_next.blank?#nil? #あんぱ？
+#					p "ないなら、prev連番"
 					 p @renban_s=@renban+1
 					 @renban_prev=@streetSide.street_name[@renban_s...@renban_end].to_i-1
 					 @name_string=@streetSide.street_name[0...@renban]
-					 #ない場合の処理を考える、次回！
 					p @renban_prev=Street.find_by(street_name: "#{@name_string}(#{@renban_prev})")
-				end
+#				else#nextもありつつprevもある
+#					 p @renban_s=@renban+1
+#					 @renban_prev=@streetSide.street_name[@renban_s...@renban_end].to_i-1
+#					 @name_string=@streetSide.street_name[0...@renban]
+#					p @renban_prev=Street.find_by(street_name: "#{@name_string}(#{@renban_prev})")
+#				end
 			end
 		@renbanAction="color:rgb(1,14,95);"
 
